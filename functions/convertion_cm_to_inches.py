173ac1e56978301edf804fa307fbbb47f2a304b4
Emil Harlem <emilaustevoll@gmail.com>
2026-02-24 11:35:37 -0500

    Notes from class 2/24

---
+++ functions/convertion_cm_to_inches.py
@@ -0,0 +1,13 @@
+
+
+def convert_height(cm):
+    """
+    Converts height from cm to feet
+    
+    Args:
+        cm(int): height in cm
+    Returns:
+        float: height in feet  
+    """
+    return(cm*0.393701)/12
+
